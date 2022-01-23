1402cd5743d28131c6cd4a1926fbdedcf001d658
Eric Noubissi <noubissi10@yahoo.fr>
2022-01-23 15:01:22 +0100

    Ajout de fetch dans la fonction getPhotographers et récupération des datas

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,42 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+/* 3- Modifier `scripts/factories/Photographer.js` 
+pour récupérer les données nécessaires (id, tagline, city, etc.) */
 
-    const picture = `assets/photographers/${portrait}`;
+function photographerFactory(data) {
+    const { id, name, portrait, city, country, tagline, price, alt } = data;
+    const picture = `./assets/photographers/PhotographersID/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        article.classList.add("photographer_card");
+        article.classList.add(id); 
+
+        const photograph = `
+        <a href="./photographer.html?id=${id}" tabindex="0">
+          <div class="photographer_img">
+            <img src="${picture}" alt="">
+            <h2>${name}</h2>
+          </div>
+        </a>
+        <div class="photographer_info" tabindex="0">
+            <p class="city">${city + ", " + country}</p>
+            <p class="tagline">${tagline}</p>
+            <p class="price">${price + "&euro;" + "/jour"}</p>
+        </div>
+        `;
+
+        article.innerHTML = photograph;
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return {
+        id,
+        name,
+        portrait,
+        city,
+        country,
+        tagline,
+        price,
+        alt,
+        getUserCardDOM,
+    }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,18 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+    function getPhotographers() {
+        // 1- Ajout de fetch dans la fonction getPhotographers 
+        // pour récupérer les datas, et faire un console.log de ces datas
+
+        fetch("./data/photographers.json")
+          .then((res) => res.json())
+          .then((res) => res.data)
+
+          .catch((error) => console.log("Il y a un error:" + error));    
     }
 
-    async function displayData(photographers) {
+    // lance l'aplication
+    getPhotographers(); 
+
+    function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
@@ -35,11 +22,17 @@
         });
     };
 
-    async function init() {
+    function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        fetch("./data/photographers.json")
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log(data);
+        const { photographers } = data;
         displayData(photographers);
-    };
+        });
+
+    }
     
     init();
     
